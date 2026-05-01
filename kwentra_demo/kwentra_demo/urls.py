bf9659dbd6ca884482161ff6a62e8c4a02c9aac0
Patrick Asaad <magyasaad424@gmail.com>
2026-05-01 15:17:09 +0300

    `Added new URL patterns and views for services and contact pages`

--- kwentra_demo/kwentra_demo/urls.py
+++ kwentra_demo/kwentra_demo/urls.py
@@ -5,6 +5,8 @@ from members import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("services/", views.services, name="services"),
+    path("contact/", views.contact, name="contact"),
     path("members/", views.members, name="members"),
     path("admin/", admin.site.urls),
 ]
--- kwentra_demo/members/views.py
+++ kwentra_demo/members/views.py
@@ -11,3 +11,11 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
