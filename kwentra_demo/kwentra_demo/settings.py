f0719a1d4f062a4304e433f400df9773fa1c8416
Patrick Asaad <magyasaad424@gmail.com>
2026-05-02 11:15:40 +0300

    `Added login and logout URLs to kwentra_demo project`

--- kwentra_demo/kwentra_demo/settings.py
+++ kwentra_demo/kwentra_demo/settings.py
@@ -19,17 +19,20 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Application definition
 
--- kwentra_demo/kwentra_demo/urls.py
+++ kwentra_demo/kwentra_demo/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from members import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -9,5 +10,11 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("members/", views.members, name="members"),
     path("admin/", admin.site.urls),
+    # Authentication
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
-handler404 = "members.views.error_404"
